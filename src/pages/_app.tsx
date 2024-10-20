561d71aec27495a8780e9fd82b32951acd139fb5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-10-20 15:39:57 +0900

    chore: 폰트 설정, 커스텀 컬러 설정, 텍스트 스타일 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import '@/styles/global.css';
 
 const queryClient = new QueryClient();
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,14 @@ export default function Home() {
         <meta name="description" content="방송인을 위한 SNS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
       </Head>
-      <div>BBB</div>
+      <div>차하</div>
     </>
   );
 }
