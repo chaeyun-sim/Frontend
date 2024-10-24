f8f7b75f9e10a4d99d35350af04b9e5986412b10
yujinyny <hyjinny00@gmail.com>
2024-10-24 20:42:39 +0900

    feat: 회원가입 api 작성

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -16,3 +16,27 @@ export const getRefresh = async (refreshToken: string) => {
 
   return response.data;
 };
+
+export const postUser = async (snsType: string, formData: FormData) => {
+  const response = await axiosInstance.post(`/members/${snsType}`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return response.data;
+};
+
+export const getCheckNickname = async (nickname: string) => {
+  const response = await axiosInstance.get(
+    `/members/check/nickname/${nickname}`
+  );
+
+  return response.data;
+};
+
+export const getServeNickname = async () => {
+  const response = await axiosInstance.get(`/members/serve/nickname`);
+
+  return response.data;
+};
