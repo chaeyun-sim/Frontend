66bc6630a151bd4a75cb32676e0861ba2d31a92b
yujinyny <hyjinny00@gmail.com>
2024-10-28 18:09:08 +0900

    style: TagInput addTag true일 때 borderBottomColor main.base 적용

--- src/components/TagInput.tsx
+++ src/components/TagInput.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 
 import Tags from './common/Tags';
+import { css, cx } from '../../styled-system/css';
 
 interface IProps {
   tagList: string[];
@@ -31,16 +32,10 @@ const TagInput = ({ tagList, setTagList }: IProps) => {
 
   return (
     <div
-      style={{
-        minHeight: '47px',
-        alignItems: 'center',
-        borderBottomColor: '#7C0DE4',
-        borderBottomWidth: '1px',
-        // width: '423px',
-        display: 'flex',
-        flexWrap: 'wrap',
-        padding: '0 12px',
-      }}
+      className={cx(
+        styles.input_container,
+        addTag ? styles.focus : styles.default
+      )}
       onClick={() => (tagList.length < 5 && !addTag ? setAddTag(true) : null)}
     >
       {tagList.map((tag) => (
@@ -65,3 +60,21 @@ const TagInput = ({ tagList, setTagList }: IProps) => {
 };
 
 export default TagInput;
+
+const styles = {
+  input_container: css({
+    minHeight: '47px',
+    alignItems: 'center',
+    borderBottomWidth: '1px',
+    // width: '423px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: '0 12px',
+  }),
+  default: css({
+    borderBottomColor: 'gray.300',
+  }),
+  focus: css({
+    borderBottomColor: 'main.base',
+  }),
+};
