b7d8f1091a51b8c9e58ad74a8c5dbf70ab5ef5c2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-10-31 22:08:15 +0900

    feat: next.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // 정적 HTML로 내보내기 설정
+  trailingSlash: true, // 선택 사항: 모든 경로에 슬래시 추가
   images: {
+    unoptimized: true, // 이미지 최적화 비활성화
     remotePatterns: [
       {
         protocol: 'https',
@@ -16,9 +19,7 @@ const nextConfig = {
         pathname: '**',
       },
     ],
-    unoptimized: true, // 이미지 최적화 비활성화
-  output: 'export', // 정적 HTML로 내보내기 설정
-  trailingSlash: true, // 선택 사항: 모든 경로에 슬래시 추가
+  },
 };
 
 export default nextConfig;
