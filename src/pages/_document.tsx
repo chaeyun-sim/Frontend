e8c00f6b679e1fc0cf846a84c1e8e419387eeffe
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-01 22:05:42 +0900

    fix: 헤드 타이틀 위치 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>방방봐</title>
         <meta name="description" content="방송인을 위한 SNS" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
