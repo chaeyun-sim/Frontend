3d989d05608fd2283710750081deebb28eb22214
yujinyny <hyjinny00@gmail.com>
2024-11-06 22:28:54 +0900

    feat: 최신 포스팅 팔로잉 리스트 컴포넌트 구현

---
+++ src/components/sns/PostingFollowings.tsx
@@ -0,0 +1,58 @@
+import FollowingProfile from './FollowingProfile';
+import { css, cx } from '../../../styled-system/css';
+
+interface IProps {
+  followings: IPostingFollowing[];
+}
+
+const PostingFollowings = ({ followings }: IProps) => {
+  return (
+    <div className={styles.container}>
+      {/* <div className={cx(styles.box, styles.left_box)} /> */}
+      <ul className={styles.list}>
+        {followings.map((v) => (
+          <li key={v.id}>
+            <FollowingProfile
+              profile={v.profile}
+              todayWords={v.todayWords}
+              isViewed={v.isViewed}
+            />
+          </li>
+        ))}
+      </ul>
+      {/* <div className={cx(styles.box, styles.right_box)} /> */}
+    </div>
+  );
+};
+
+export default PostingFollowings;
+
+const styles = {
+  container: css({
+    position: 'relative',
+    width: '620px',
+  }),
+  list: css({
+    display: 'flex',
+    gap: '8px',
+    overflowX: 'auto',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+  }),
+  box: css({
+    position: 'absolute',
+    top: 0,
+    width: '48px',
+    height: '100%',
+    zIndex: 10,
+  }),
+  left_box: css({
+    left: 0,
+    background: 'linear-gradient(to left, transparent, white)',
+  }),
+  right_box: css({
+    right: 0,
+    background: 'linear-gradient(to right, transparent, white)',
+  }),
+};
