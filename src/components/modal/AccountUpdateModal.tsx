df69be0944e57778acb692a724fb5be953dfd682
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-11-27 20:20:14 +0900

    feat: Modal을 모달 컴포넌트 내부로 이동

--- src/components/modal/AccountUpdateModal.tsx
+++ src/components/modal/AccountUpdateModal.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { css } from '../../../styled-system/css';
 import { flex } from '../../../styled-system/patterns';
 import Button from '../common/Button';
+import Modal from '../common/Modal';
 
 interface IProps {
   onClose: () => void;
@@ -10,63 +11,65 @@ interface IProps {
 
 const AccountUpdateModal = ({ onClose }: IProps) => {
   return (
-    <div>
-      <div className={styles.modal_title}>방송인 계정 등업</div>
-      <span
-        className={css({
-          padding: '0 20px',
-          textStyle: 'body4',
-          color: 'gray.900',
-        })}
-      >
-        방송인 계정으로 등업하시겠습니까?
-      </span>
-      <div className={css({ padding: '8px 20px' })}>
-        <div
+    <Modal className={css({ width: '480px' })} onClose={onClose}>
+      <div>
+        <div className={styles.modal_title}>방송인 계정 등업</div>
+        <span
           className={css({
-            padding: '12px',
-            backgroundColor: 'gray.50',
-            borderRadius: '8px',
+            padding: '0 20px',
             textStyle: 'body4',
             color: 'gray.900',
           })}
         >
-          <p>1. 안내사항 </p>
-          <p>2. 안내사항</p>
-          <p>3. 안내사항</p>
+          방송인 계정으로 등업하시겠습니까?
+        </span>
+        <div className={css({ padding: '8px 20px' })}>
+          <div
+            className={css({
+              padding: '12px',
+              backgroundColor: 'gray.50',
+              borderRadius: '8px',
+              textStyle: 'body4',
+              color: 'gray.900',
+            })}
+          >
+            <p>1. 안내사항 </p>
+            <p>2. 안내사항</p>
+            <p>3. 안내사항</p>
+          </div>
         </div>
-      </div>
-      <div className={css({ padding: '20px' })}>
+        <div className={css({ padding: '20px' })}>
+          <div
+            className={css({
+              padding: '8px',
+              borderRadius: '4px',
+              borderColor: 'gray.300',
+              borderWidth: '1px',
+              textStyle: 'button2',
+              color: 'gray.700',
+              wordBreak: 'break-all',
+            })}
+          >
+            https://www.figma.com/design/OrshYtN8dGIWucxseBLY1X/ProjectBBB?node-id=685-6588&t=B8blJBA3lCu1xlzt-1
+          </div>
+        </div>
         <div
-          className={css({
-            padding: '8px',
-            borderRadius: '4px',
-            borderColor: 'gray.300',
-            borderWidth: '1px',
-            textStyle: 'button2',
-            color: 'gray.700',
-            wordBreak: 'break-all',
-          })}
+          className={flex({ padding: '20px', justifyContent: 'space-between' })}
         >
-          https://www.figma.com/design/OrshYtN8dGIWucxseBLY1X/ProjectBBB?node-id=685-6588&t=B8blJBA3lCu1xlzt-1
+          <Button
+            variant="outlined"
+            text="취소"
+            className={css({ width: '216px' })}
+            onClick={onClose}
+          />
+          <Button
+            variant="contained"
+            text="확인"
+            className={css({ width: '216px' })}
+          />
         </div>
       </div>
-      <div
-        className={flex({ padding: '20px', justifyContent: 'space-between' })}
-      >
-        <Button
-          variant="outlined"
-          text="취소"
-          className={css({ width: '216px' })}
-          onClick={onClose}
-        />
-        <Button
-          variant="contained"
-          text="확인"
-          className={css({ width: '216px' })}
-        />
-      </div>
-    </div>
+    </Modal>
   );
 };
 
--- src/components/modal/ShowFollowersModal.tsx
+++ src/components/modal/ShowFollowersModal.tsx
@@ -2,35 +2,41 @@ import React from 'react';
 
 import { css } from '../../../styled-system/css';
 import Button from '../common/Button';
+import Modal from '../common/Modal';
 import PersonBox from '../PersonBox';
 
 interface IProps {
-  onCloseModal: () => void;
+  onClose: () => void;
   type: 'follower' | 'following' | null;
 }
 
-const ShowFollowersModal = ({ onCloseModal, type }: IProps) => {
+const ShowFollowersModal = ({ onClose, type }: IProps) => {
   return (
-    <div style={{ position: 'relative' }}>
-      <div className={styles.modal_title}>
-        {type === 'follower' ? '팔로워' : '팔로잉'}
-      </div>
-      <div className={styles.modal_content}>
-        <div className={styles.inner_content}>
-          {['이민정', '김민정', '최민정', '연민정'].map((name) => (
-            <PersonBox key={name} data={name} keyword="" onClick={() => {}} />
-          ))}
+    <Modal
+      onClose={onClose}
+      className={css({ width: '360px', height: '524px' })}
+    >
+      <div style={{ position: 'relative' }}>
+        <div className={styles.modal_title}>
+          {type === 'follower' ? '팔로워' : '팔로잉'}
         </div>
+        <div className={styles.modal_content}>
+          <div className={styles.inner_content}>
+            {['이민정', '김민정', '최민정', '연민정'].map((name) => (
+              <PersonBox key={name} data={name} keyword="" onClick={() => {}} />
+            ))}
+          </div>
+        </div>
+        <Button
+          text="확인"
+          className={css({
+            width: 'calc(100% - 40px)',
+            margin: '20px',
+          })}
+          onClick={onClose}
+        />
       </div>
-      <Button
-        text="확인"
-        className={css({
-          width: 'calc(100% - 40px)',
-          margin: '20px',
-        })}
-        onClick={onCloseModal}
-      />
-    </div>
+    </Modal>
   );
 };
 
