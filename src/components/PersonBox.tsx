05423cf6c077556be4fbe34138addf96d80c838a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-17 20:33:01 +0900

    fix: 더미 이미지 삭제, 이미지 관련 에러 수정

--- src/components/PersonBox.tsx
+++ src/components/PersonBox.tsx
@@ -37,9 +37,7 @@ const PersonBox = ({ data, keyword, onClick, hasAdd }: IProps) => {
     <div className={styles.item}>
       <div className={styles.image}>
         <Image
-          src={
-            'https://images.unsplash.com/photo-1730343464315-a9ca01f9f1c6?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          }
+          src={''}
           alt="profile"
           width={36}
           height={36}
