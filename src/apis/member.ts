005df4f2cbbd271bbdd60de2e2ae1c00496fecdf
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-20 00:15:30 +0900

    feat: API 반환 값 수정

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -7,35 +7,35 @@ export const checkIdMatch = async (memberId: string) => {
 
 export const getProfileSummary = async (memberId: string) => {
   const response = await authInstance.get(`/members/summary/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const getProfileInfo = async (memberId: string) => {
   const response = await authInstance.get(`/members/profile/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const getPostList = async (memberId: string) => {
   const response = await authInstance.get(`/members/posts/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const getFollowers = async (memberId: string) => {
   const response = await authInstance.get(`/members/followers/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const getFollows = async (memberId: string) => {
   const response = await authInstance.get(`/members/follows/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const getComments = async (memberId: string) => {
   const response = await authInstance.get(`/members/comments/${memberId}`);
-  return response.data;
+  return response.data.data;
 };
 
 export const postFollow = async (data: IPostFollowReq) => {
   const response = await authInstance.post('/members/toggleFollow', data);
-  return response.data;
+  return response.data.data;
 };
