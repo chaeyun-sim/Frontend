6d589c9ffdcc2670019aa6b91848ba2ca0655f33
yujinyny <hyjinny00@gmail.com>
2024-12-19 23:22:33 +0900

    feat: HeaderProfileMenu 컴포넌트 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,30 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { useToggle } from '@/hooks/useToggle';
 import { useAuth } from '@/stores/useAuth';
 import { useUserStore } from '@/stores/useUserStore';
 
+import ProfileMenu from './HeaderProfileMenu';
 import Icon from './Icon';
 import Modal from './Modal';
-import Switch from './Switch';
 import { css } from '../../../styled-system/css';
 import LoginModal from '../modal/LoginModal';
 
 const Header = () => {
-  const { isLoggedIn, logout } = useAuth();
-  const { role, setUserRole } = useUserStore();
-  const { isOpen: isOnRoleSwitch, handleToggle: handleToggleRoleSwitch } =
-    useToggle(role === 'STREAMER');
+  const { isLoggedIn, profileImage } = useAuth();
+  const { role } = useUserStore();
+
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const { isOpen: isOpenProfileMenu, handleToggle: handleToggleProfileMenu } =
+    useToggle(false);
 
   const openModal = () => setIsLoginModalOpen(true);
   const closeModal = () => setIsLoginModalOpen(false);
-
-  useEffect(() => {
-    if (isOnRoleSwitch) setUserRole('STREAMER');
-    else setUserRole('MEMBER');
-  }, [isOnRoleSwitch]);
 
   return (
     <>
@@ -45,16 +42,25 @@ const Header = () => {
             <Link href={`/sns/${role === 'MEMBER' ? 'normal' : 'broadcast'}`}>
               SNS 이동
             </Link>
-            {isLoggedIn && (
-              <Switch
-                label="방송모드"
-                on={isOnRoleSwitch}
-                handleToggle={handleToggleRoleSwitch}
-              />
+            {isLoggedIn ? (
+              <div className={styles.profile_container}>
+                <Image
+                  src={profileImage}
+                  alt="프로필"
+                  width={36}
+                  height={36}
+                  onClick={handleToggleProfileMenu}
+                  className={css({ cursor: 'pointer' })}
+                />
+                {isOpenProfileMenu && (
+                  <div className={styles.profile_menu_container}>
+                    <ProfileMenu handleToggle={handleToggleProfileMenu} />
+                  </div>
+                )}
+              </div>
+            ) : (
+              <button onClick={openModal}>로그인</button>
             )}
-            <button onClick={isLoggedIn ? logout : openModal}>
-              {isLoggedIn ? '로그아웃' : '로그인'}
-            </button>
           </div>
         </div>
       </div>
@@ -83,5 +89,14 @@ const styles = {
     gap: 36,
     alignItems: 'center',
     textStyle: 'button1',
+  }),
+  profile_container: css({
+    position: 'relative',
+  }),
+  profile_menu_container: css({
+    position: 'absolute',
+    top: 'calc(100% + 10px)',
+    right: 0,
+    zIndex: 2,
   }),
 };
---
+++ src/components/common/HeaderProfileMenu.tsx
@@ -0,0 +1,71 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import { useToggle } from '@/hooks/useToggle';
+import { useAuth } from '@/stores/useAuth';
+import { useUserStore } from '@/stores/useUserStore';
+
+import Switch from './Switch';
+import { css } from '../../../styled-system/css';
+
+interface IProps {
+  handleToggle: () => void;
+}
+
+const HeaderProfileMenu = ({ handleToggle }: IProps) => {
+  const router = useRouter();
+
+  const { logout } = useAuth();
+  const { role, setUserRole } = useUserStore();
+  const { isOpen: isOnRoleSwitch, handleToggle: handleToggleRoleSwitch } =
+    useToggle(role === 'STREAMER');
+
+  const handleRedirectMypage = () => {
+    handleToggle();
+    router.push('mypage');
+  };
+
+  useEffect(() => {
+    if (isOnRoleSwitch) setUserRole('STREAMER');
+    else setUserRole('MEMBER');
+  }, [isOnRoleSwitch]);
+
+  return (
+    <ul className={styles.list}>
+      <li className={styles.item}>
+        <Switch
+          label="방송모드"
+          on={isOnRoleSwitch}
+          handleToggle={handleToggleRoleSwitch}
+        />
+      </li>
+      <li onClick={handleRedirectMypage} className={styles.item}>
+        내 정보
+      </li>
+      <li onClick={logout} className={styles.item}>
+        로그아웃
+      </li>
+    </ul>
+  );
+};
+
+export default HeaderProfileMenu;
+
+const styles = {
+  list: css({
+    paddingY: 12,
+    width: 120,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: 8,
+    backgroundColor: 'white',
+  }),
+  item: css({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
+    cursor: 'pointer',
+  }),
+};
